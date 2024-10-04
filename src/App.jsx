ef2f80d42e8cf11eadc37bea4cc2386c65aab234
bac00001 <113459832+bac00001@users.noreply.github.com>
2024-10-03 22:57:42 -0400

    add CrewPage structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,22 @@ import {
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import DestinationPage, { destinationLoader } from './pages/DestinationPage';
-
-import { dataLoader } from './lib/utils';
+import CrewPage, { crewLoader } from './pages/CrewPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
+
     {/* Redirect to '/destination/Moon' by default when visiting '/destination' */}
     <Route path='/destination' element={< Navigate to='/destination/Moon' />} />
     {/* Dynamic route for destination based on destination name using React router's data loader */}
     <Route path='/destination/:name' element={< DestinationPage />} loader={destinationLoader} />
+
+    {/* Redirect to '/crew/douglashurley' by default when visiting '/crew' */}
+    <Route path='/crew' element={< Navigate to='/crew/douglas-hurley' />} />
+    {/* Dynamic route for crew based on their name using React router's data loader */}
+    <Route path='/crew/:name' element={< CrewPage />} loader={crewLoader} />
   </Route>
 )
 )
--- src/pages/CrewPage.jsx
+++ src/pages/CrewPage.jsx
@@ -1,33 +1,33 @@
 import { NavLink, useLoaderData } from "react-router-dom";
-import moonImg from '../assets/destination/image-moon.webp';
-import marsImg from '../assets/destination/image-mars.webp';
-import europaImg from '../assets/destination/image-europa.webp';
-import titanImg from '../assets/destination/image-titan.webp';
+import hurleyImg from '../assets/crew/image-douglas-hurley.webp';
+import shuttleworthImg from '../assets/crew/image-mark-shuttleworth.webp';
+import gloverImg from '../assets/crew/image-victor-glover.webp';
+import ansariImg from '../assets/crew/image-anousheh-ansari.webp';
 import { dataLoader } from "../lib/utils";
 
-const DestinationPage = () => {
-    const destination = useLoaderData(); // Get the data of the destination fetched by dataLoader function
-    // console.log(destination.name);
+const CrewPage = () => {
+    const crew = useLoaderData(); // Get the data of the crew fetched by dataLoader function
 
     // Since the path in the mock api (from data.json file) is relative, we cannot access the img from this file
     // So, the imgs are imported, and the one matching the destination name will be displayed
     const images = {
-        'Moon': moonImg,
-        'Mars': marsImg,
-        'Europa': europaImg,
-        'Titan': titanImg,
+        'douglas-hurley': hurleyImg,
+        'mark-shuttleworth': shuttleworthImg,
+        'victor-glover': gloverImg,
+        'anousheh-ansari': ansariImg,
     };
 
     return (
-        <div className="d-flex-sm-column main-container destination-main-container">
+        <div className="d-flex-sm-column main-container crew-main-container">
             <div className="d-flex page-heading text-preset-6">
-                <strong className="color-light-primary">01</strong>&nbsp;Pick your destination
+                <strong className="color-light-primary">01</strong>&nbsp;Meet Your Crew
             </div>
             <div className="d-flex-sm-column content-container">
-                <div className="d-flex-sm-column destination-img">
-                    <img src={images[destination.name]} alt={`Image of ${destination.name}`} />
-                </div>
-                <div className="d-flex-sm-column destination-info-container">
+                <div className="d-flex-sm-column crew-info-container">
+                    <div className="d-flex-sm-column text-container">
+                        <div className="title text-preset-2">{crew.name}</div>
+                        <div className="description text-preset-9 color-secondary">{crew.description}</div>
+                    </div>
                     <div className="d-flex tab-container text-preset-8">
                         {/* Add active classname for active tab styling */}
                         <NavLink
@@ -55,12 +55,10 @@ const DestinationPage = () => {
                             Titan
                         </NavLink>
                     </div>
-                    <div className="d-flex-sm-column text-container">
-                        <div className="title text-preset-2">{destination.name}</div>
-                        <div className="description text-preset-9 color-secondary">{destination.description}</div>
-                        <div className="line"></div>
+                    <div className="d-flex-sm-column destination-img">
+                        <img src={images[crew.name]} alt={`Image of ${crew.name}, ${crew.role}`} />
                     </div>
-                    <div className="d-flex-sm-column stats-container">
+                    {/* <div className="d-flex-sm-column stats-container">
                         <div className="d-flex-sm-column distance-stats">
                             <div className="distance-heading text-preset-7 color-secondary">Avg. distance</div>
                             <div className="distance text-preset-10">{destination.distance}</div>
@@ -69,22 +67,14 @@ const DestinationPage = () => {
                             <div className="travel-time-heading text-preset-7 color-secondary">Est. travel time</div>
                             <div className="travel-time text-preset-10">{destination.travel}</div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
     )
 };
 
-// ***Function moved to utils.jsx for reusibility
-// Function to fetch the data of the relevant destination (based on destination name)
-// const dataLoader = async ({params}) => {
-//     const res = await fetch(`http://localhost:8000/destinations?name=${params.name}`);
-//     const data = await res.json();
-//     return data[0]; // Since the result is an array, return the first matching item (there should be only one)
-// };
-
 // Use dataLoader utility function to fetch the data of the relevant destination (based on destination name parameter in the url)
-const destinationLoader = (params) => dataLoader(params, 'http://localhost:8000/destinations');
+const crewLoader = (params) => dataLoader(params, 'http://localhost:8000/crew');
 
-export { DestinationPage as default, destinationLoader };
+export { CrewPage as default, crewLoader };
