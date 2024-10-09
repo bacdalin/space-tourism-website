d25f0e004ac9104ad52bc8d46b0db02e376c6ba6
bac00001 <113459832+bac00001@users.noreply.github.com>
2024-10-09 11:32:43 -0400

    add mobile styling for CrewPage

--- src/pages/CrewPage.jsx
+++ src/pages/CrewPage.jsx
@@ -17,14 +17,6 @@ const CrewPage = () => {
         'Anousheh Ansari': ansariImg,
     };
 
-    // **Feature remove to minimize code
-    // Check if "name" is a single word or has space (multi-word)
-    // If yes, replace space with hyphen and turn all caracters to lowercase
-    // If no, turn all characters to lowercase
-    // const slugName = crew.name.includes(' ')
-    //     ? crew.name.split(' ').map(word => word.charAt(0).toLowerCase() + word.slice(1)).join('-')
-    //     : crew.name.charAt(0).toLowerCase() + crew.name.slice(1);
-
     return (
         <div className="d-flex-sm-column main-container main-container-wgap crew-main-container">
             <div className="d-flex page-heading text-preset-6">
@@ -39,33 +31,33 @@ const CrewPage = () => {
                         </div>
                         <div className="bio text-preset-9 color-secondary">{crew.bio}</div>
                     </div>
-                </div>
-                <div className="d-flex tab-container text-preset-8">
-                    {/* Add active classname for active tab styling */}
-                    <NavLink
-                        to='/crew/douglas-hurley' 
-                        className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
-                    >
-                        <div className="dot-tab"></div>
-                    </NavLink> 
-                    <NavLink 
-                        to='/crew/mark-shuttleworth'
-                        className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
-                    >
-                        <div className="dot-tab"></div>
-                    </NavLink>
-                    <NavLink 
-                        to='/crew/victor-glover'
-                        className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
-                    >
-                        <div className="dot-tab"></div>
-                    </NavLink>
-                    <NavLink 
-                        to='/crew/anousheh-ansari'
-                        className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
-                    >
-                        <div className="dot-tab"></div>
-                    </NavLink>
+                    <div className="d-flex tab-container text-preset-8">
+                        {/* Add active classname for active tab styling */}
+                        <NavLink
+                            to='/crew/douglas-hurley' 
+                            className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
+                        >
+                            <div className="dot-tab"></div>
+                        </NavLink> 
+                        <NavLink 
+                            to='/crew/mark-shuttleworth'
+                            className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
+                        >
+                            <div className="dot-tab"></div>
+                        </NavLink>
+                        <NavLink 
+                            to='/crew/victor-glover'
+                            className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
+                        >
+                            <div className="dot-tab"></div>
+                        </NavLink>
+                        <NavLink 
+                            to='/crew/anousheh-ansari'
+                            className={({isActive}) => isActive ? "tab-item active-tab" : "tab-item"}
+                        >
+                            <div className="dot-tab"></div>
+                        </NavLink>
+                    </div>
                 </div>
                 <div className="d-flex-sm-column crew-img">
                     <img src={images[crew.name]} alt={`Image of ${crew.name}, ${crew.role}`} />
